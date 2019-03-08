31c44f7322162e9ca1db794a6d2c6c6f664cb386
ZhangYi520 <1073988580@qq.com>
2019-03-08 17:26:33 +0800

    commit1

---
+++ sso/src/main/java/com/zy/sso/controller/demo.java
@@ -0,0 +1,13 @@
+package com.zy.sso.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demo {
+
+	@GetMapping("/test")
+	public String to() {
+		return "aaaaa";
+	}
+}
