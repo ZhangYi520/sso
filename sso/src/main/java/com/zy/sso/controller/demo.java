5fb11d22d496771f55abc8f12b1eee2eb7ad74c9
ZhangYi520 <1073988580@qq.com>
2019-07-16 10:31:53 +0800

    idea demo测试

--- sso/src/main/java/com/zy/sso/controller/demo.java
+++ sso/src/main/java/com/zy/sso/controller/demo.java
@@ -1,32 +1,29 @@
 package com.zy.sso.controller;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.authz.annotation.RequiresRoles;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.zy.sso.base.result.Result;
+
 
 @RestController
 public class demo {
-	
 	@GetMapping("/test")
 	@RequiresRoles("超级管理员")//shiro角色注解，拥有该角色才能访问
 	@RequiresPermissions("/select")//shiro权限注解，拥有该权限才能访问
 	public String to() {
 		return "idea";
 	}
-	
+
 	@GetMapping("/test2")
 	@RequiresPermissions("/delete")
 	public String to2() {
 		return "2019年4月28日11:21:48";
 	}
-	
+
 	@GetMapping("/test3")
 	@RequiresRoles("超级管理员")//shiro角色注解，拥有该角色才能访问
 	@RequiresPermissions("/s")
@@ -57,28 +54,15 @@ public class demo {
 ////		System.out.println(selectList);
 //		return 1;
 //	}
-
-	public static void main (String [] args){
-
-	}
-	class Solution{
-		public ListNode FindKthToTail(ListNode head,int k) {
-			return null;
-		}
-	}
-
-
-	class ListNode{
-		int val;
-		ListNode next;
-
-		public int getVal() {
-			return val;
-		}
-
-		public void setVal(int val) {
-			this.val = val;
-		}
-	}
-
 }
+
+class 类{
+	private static String 名字;
+	private static String 性别;
+	public static void main(String [] args){
+		名字="小毅毅";
+		性别="男";
+		System.out.println(名字+性别);
+	}
+}
+
