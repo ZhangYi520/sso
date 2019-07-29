5683656143f5346b22a2445b2ce1c5828ec63c42
ZhangYi520 <zzzyyy000>
2019-07-29 17:25:14 +0800

    路径跳转

---
+++ sso/src/main/java/com/zy/sso/systemManage/ToSystemViewController.java
@@ -0,0 +1,15 @@
+package com.zy.sso.systemManage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/system")
+public class ToSystemViewController {
+
+    @GetMapping("/toUser")
+    public String toUser(){
+        return "/system/user/user";
+    }
+}
